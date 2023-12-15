4c820acfc7bc7509a0ae67a67c3ada6f16fd0e75
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-15 17:13:42 +0100

    done

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Mehrwertsteuer-Rechner
+
+# Dynamic VAT calculator 
+
+This JavaScript code powers a dynamic VAT calculator with two radio buttons for switching between gross and net prices. The code listens for changes in the radio buttons and updates the displayed values accordingly. The vat function calculates VAT and total values based on user input, considering different tax rates. The associated HTML elements are targeted using their IDs for efficient interaction.
+
+## Authors
+
+- [@StefanAiroaie](https://www.github.com/StefanAiroaie)
+
+
+## Features
+
+Based on the provided JavaScript code, it seems that the calculator has the following features:
+
+1. **Dynamic Title Update:**
+   - When the "Netto to Brutto" radio button is selected, the title is updated to "Bruttobetrag (Endpreis)".
+   - When the "Brutto to Netto" radio button is selected, the title is updated to "Nettobetrag".
+
+2. **VAT Calculation:**
+   - The `vat` function calculates VAT and total values based on user input.
+   - Different scenarios are considered based on whether the user chooses netto or brutto and the selected VAT rate (19% or 7%).
+
+3. **Event Listeners:**
+   - Event listeners are used to trigger actions when the radio buttons ("nettoBrutto" and "bruttoNetto") are changed.
+
+4. **User Input Handling:**
+   - User input is obtained from an input element with the ID "inputValue".
+
+5. **Display Update:**
+   - The results are displayed in HTML elements with the IDs "vatValue" and "totalValueOrNetto".
+
+
+
+
+## Use
+
+https://stefanairoaie.github.io/Mehrwertsteuer-Rechner/
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,22 @@
 
-let bruttoNetto = document.getElementById("bruttoNetto").checked
-let nettoBrutto = document.getElementById("nettoBrutto").checked
-let vatValue = document.getElementById("vatValue")
-let totalValueOrNettoTitle = document.getElementById("totalValueOrNettoTitle")
-let totalValueOrNetto = document.getElementById("totalValueOrNetto")
-let inputValueLabel = document.getElementById("inputValueLabel")
-
-
+//######### events text andern
 document.getElementById("nettoBrutto").addEventListener("change", () => {
-    totalValueOrNettoTitle.innerHTML = "Bruttobetrag (Endpreis)";
+    totalValueOrNettoTitle.innerHTML = "Bruttobetrag (Endpreis)"
     inputValueLabel.innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro <span id='star'>*</span>"
-    ImportType = "nettoBrutto"
 })
 
 document.getElementById("bruttoNetto").addEventListener("change", () => {
     console.log("bruttoNetto")
     totalValueOrNettoTitle.innerHTML = "Nettobetrag"
     inputValueLabel.innerHTML = "Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro <span id='star'>*</span>"
-    ImportType = "bruttoNetto"
 })
 
-
-
-
-
+//######### on click function
 const vat = () => {
-    // alle notwendige variable deklariert
 
+    let nettoBrutto = document.getElementById("nettoBrutto").checked
+    let vatValue = document.getElementById("vatValue")
+    let totalValueOrNetto = document.getElementById("totalValueOrNetto")
     let vat19 = document.getElementById("vat19").checked
     let vat7 = document.getElementById("vat7").checked
     let inputValue = document.getElementById("inputValue").value
@@ -36,21 +26,25 @@ const vat = () => {
     if (nettoBrutto == true && vat19 == true) {
         vatValue.innerHTML = (inputValue * 0.19).toFixed(2) + " €"
         totalValueOrNetto.innerHTML = (inputValue * 1.19).toFixed(3) + " €"
+        console.log("1");
     }
     // wenn netto to bruto und 7%
     else if (nettoBrutto == true && vat7 == true) {
         vatValue.innerHTML = Math.round(inputValue * 0.07).toFixed(2) + " €"
         totalValueOrNetto.innerHTML = Math.round(inputValue * 1.07).toFixed(2) + " €"
+        console.log("2");
     }
     // wenn bruto to netto und 19%
     else if (nettoBrutto == false && vat19 == true) {
         vatValue.innerHTML = (inputValue - inputValue / (1 + 19 / 100)).toFixed(2) + " €"
         totalValueOrNetto.innerHTML = (inputValue / (1 + 19 / 100)).toFixed(2) + " €"
+        console.log("3");
     }
     // wenn bruto to netto und 7%
     else if (nettoBrutto == false && vat7 == true) {
         vatValue.innerHTML = (inputValue - inputValue / (1 + 7 / 100)).toFixed(2) + " €"
         totalValueOrNetto.innerHTML = (inputValue / (1 + 7 / 100)).toFixed(2) + " €"
+        console.log("4");
     }
 }
 
